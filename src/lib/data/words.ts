9da6987fb011f232c5fef0209b7b73b3c96be945
James Best <james.best@breedr.co>
2025-07-29 23:04:03 +0100

    feat(content): update spelling word curriculum
    - Replace existing word list with targeted spelling curriculum
    - Add challenging words with complex patterns (accident/accidentally, etc.)
    - Include British spelling variations and difficult vocabulary
    - Focus on ~100 high-value educational words

--- src/lib/data/words.ts
+++ src/lib/data/words.ts
@@ -1,53 +1,20 @@
-// Year 3 spelling words for ages 7-8 (300 words organized by difficulty and frequency)
+// Spelling curriculum words
 export const YEAR_3_WORDS: string[] = [
-  // High frequency words (1-50)
-  "about", "after", "again", "against", "also", "always", "another", "any", 
-  "anyone", "anything", "around", "ask", "asked", "asking", "because", "been", 
-  "before", "best", "better", "between", "both", "boy", "boys", "brother", 
-  "called", "came", "can't", "cat", "cats", "children", "class", "come", 
-  "coming", "could", "day", "days", "did", "didn't", "different", "does", 
-  "don't", "door", "down", "each", "end", "even", "every", "everyone", 
-  "everything", "family",
-
-  // Common nouns and verbs (51-100)
-  "father", "find", "first", "found", "friend", "friends", "from", "get", 
-  "girl", "girls", "give", "go", "going", "good", "got", "great", "group", 
-  "hand", "hands", "head", "help", "her", "here", "him", "his", "home", 
-  "house", "how", "I'll", "I'm", "idea", "if", "important", "into", "it's", 
-  "its", "job", "keep", "kept", "know", "last", "left", "let", "life", 
-  "little", "live", "long", "look", "looking", "lot",
-
-  // Action words and descriptive words (101-150)
-  "made", "make", "making", "man", "may", "me", "might", "money", "more", 
-  "most", "mother", "move", "much", "must", "name", "need", "never", "new", 
-  "next", "night", "nothing", "now", "number", "of", "off", "often", "old", 
-  "once", "only", "open", "or", "other", "our", "out", "over", "own", "part", 
-  "people", "place", "put", "right", "room", "said", "same", "saw", "say", 
-  "school", "see", "should", "show",
-
-  // Advanced vocabulary (151-200)
-  "side", "since", "small", "some", "something", "still", "such", "take", 
-  "taken", "tell", "than", "that", "their", "them", "then", "there", "these", 
-  "they", "thing", "think", "this", "those", "thought", "three", "through", 
-  "time", "today", "together", "too", "took", "turn", "two", "under", "until", 
-  "up", "us", "use", "used", "using", "very", "want", "water", "way", "we", 
-  "week", "well", "went", "were", "what", "where",
-
-  // Additional essential words (201-250)
-  "while", "who", "why", "will", "with", "without", "woman", "work", "world", 
-  "would", "write", "year", "yes", "yet", "you", "young", "your", "able", 
-  "above", "across", "add", "almost", "along", "already", "although", "animal", 
-  "answer", "appear", "area", "away", "back", "bad", "ball", "beautiful", 
-  "become", "began", "begin", "being", "believe", "below", "big", "black", 
-  "blue", "book", "bring", "build", "buy",
-
-  // Subject-specific and curriculum words (251-300)
-  "change", "city", "clean", "clear", "close", "cold", "colour", "complete", 
-  "country", "course", "cut", "dark", "decide", "deep", "dog", "done", "draw", 
-  "during", "early", "earth", "easy", "eat", "enough", "example", "face", 
-  "fact", "fall", "far", "fast", "feel", "few", "field", "fine", "fire", 
-  "fish", "five", "follow", "food", "form", "four", "free", "full", "game", 
-  "gave", "general", "glass", "green", "ground", "grow", "half", "happen", 
-  "hard", "hear", "heart", "heavy", "high", "hold", "hope", "hot", "hour", 
-  "however", "human", "hundred", "ice", "include", "inside", "instead", "just"
+  "accident", "accidentally", "actual", "actually", "address", "answer", 
+  "appear", "arrive", "believe", "bicycle", "breath", "breathe", "build", 
+  "busy", "business", "calendar", "caught", "centre", "century", "certain", 
+  "circle", "complete", "consider", "continue", "decide", "describe", 
+  "different", "difficult", "disappear", "early", "earth", "eight", "eighth", 
+  "enough", "exercise", "experience", "experiment", "extreme", "famous", 
+  "favourite", "February", "forward", "forwards", "fruit", "grammar", "group", 
+  "guard", "guide", "heard", "heart", "height", "history", "imagine", 
+  "increase", "important", "interest", "island", "knowledge", "learn", 
+  "length", "library", "material", "medicine", "mention", "minute", "natural", 
+  "naughty", "notice", "occasion", "occasionally", "often", "opposite", 
+  "ordinary", "particular", "peculiar", "perhaps", "popular", "position", 
+  "possess", "possession", "possible", "potatoes", "pressure", "probably", 
+  "promise", "purpose", "quarter", "question", "recent", "regular", "reign", 
+  "remember", "sentence", "separate", "special", "straight", "strange", 
+  "strength", "suppose", "surprise", "therefore", "though", "although", 
+  "thought", "through", "various", "weight", "woman", "women"
 ];
